d5c98bdd3e2078c8847fa6ae2f300c7a2bf7d4fb
mariomeissner <mr.rash.boy@gmail.com>
2018-10-14 13:34:13 +0200

    Can draw points thorugh console

--- browser_paint/main.js
+++ browser_paint/main.js
@@ -2,6 +2,134 @@ canvas = document.querySelector(".canvas");
 cm = new CanvasManager(canvas);
 p = new Painter(cm);
 
+function Painter(cm) {
+
+    var actions = [];
+    configure();
+    drawMode = "point";
+
+    function clickEvent(x, y) {
+        switch (drawMode) {
+            case "point":
+                drawPoint(x, y);
+                break;
+
+            case "line":
+
+                break;
+
+            default:
+                break;
+        }
+    }
+    function drawPoint(x, y) {
+        cm.point(x, y);
+        point = new Point(x, y);
+        addAction(point);
+    }
+
+    this.testLine = function(x1, y1, x2, y2) {
+        drawLine(x1, y1, x2, y2)
+    }
+
+    function drawLine(x1, y1, x2, y2) {
+
+        pointList = Algorithms.slopeIntersect(x1, y1, x2, y2);
+        console.log(pointList);
+        for (var i = 0; i < pointList.length; i++) {
+            cm.point(pointList[i].x, pointList[i].y);
+        }
+        line = new Line(pointList[0], pointList[pointList.length - 1]);
+        addAction(line);
+    }
+
+    function addAction(action) {
+        actions.push(action);
+        document.getElementById("actions-box").innerText +=
+            action.toString() + "\n";
+    }
+
+
+
+
+    function configure() {
+
+        // Listen for mouse clicks over canvas
+        cm.canvas.addEventListener('click', function (event) {
+            var pos = cm.getMouseCoordinates(event);
+            clickEvent(pos.x, pos.y);
+        })
+
+        // Listen for mouse movements over canvas and update info box
+        canvas.addEventListener('mousemove', function (event) {
+            var pos = cm.getMouseCoordinates(event);
+            var message = '(' + pos.x + ', ' + pos.y + ")";
+            document.getElementById('mouse-box').innerHTML = message
+
+        }, false);
+
+        // Button listeners
+        document.getElementById("axis").addEventListener("click", cm.setAxisVisibility);
+        document.getElementById("point-mode").addEventListener("click", function () {
+            drawMode = "point";
+        });
+        document.getElementById("line-mode").addEventListener("click", function () {
+            drawMode = "line";
+        });
+
+    }
+}
+
+class Algorithms {
+
+    static slopeIntersect(x1, y1, x2, y2) {
+
+        var list = [];
+    
+        // If first point farther right than last one, run backwards
+        if (x1 > x2) {
+            list = Algorithms.slopeIntersect(x2, y2, x1, y1)
+        }
+    
+        // If vertical 
+        else if (x2 - x1 == 0) {
+    
+            // If first point above last one, run backwards
+            if (y1 > y2) {
+                list = Algorithms.slopeIntersect(x2, y2, x1, y1);
+            } else {
+                for (var y = y1; y <= y2; y++) {
+                    list.push(x1, y);
+                }
+                return list;
+            }
+    
+        } else {
+            var m = (y2 - y1) / (x2 - x1);
+            var b = y1 - m * x1;
+            // If slope higher than 45 deg, flip axis
+            if (m > 1) {
+                list = Algorithms.flipAxis(Algorithms.slopeIntersect(y1, x1, y2, x2))
+            } else {
+                for (var x = x1; x <= x2; x++) {
+                    list.push(new Point(x, Math.round(m * x + b)));
+                }
+            }
+        }
+
+        return list;
+    }
+    
+    static flipAxis(pointList) {
+        for (var i = 0; i < pointList.length; i++) {
+            var temp = pointList[i].x;
+            pointList[i].x = pointList[i].y;
+            pointList[i].y = temp;
+        }
+        return pointList;
+    }
+}
+
 function CanvasManager(canvas) {
 
     // Offer public access to canvas
@@ -81,143 +209,20 @@ function CanvasManager(canvas) {
 }
 
 
+// SHAPES
 
-function Painter(cm) {
-
-    var actions = [];
-    configure();
-    drawMode = "point";
-
-    function clickEvent(x, y) {
-        switch (drawMode) {
-            case "point":
-                drawPoint(x, y);
-                break;
-
-            case "line":
-
-                break;
-
-            default:
-                break;
+function Line(first, last) {
+        this.first = first;
+        this.last = last;
+        this.toString = function() {
+            return "Line(("+ this.first.x + ", " + this.first.y + ") -> (" + this.last.x + ", " + this.last.y + "))";
         }
-    }
-    function drawPoint(x, y) {
-        cm.point(x, y);
-        point = new Point(x, y);
-        addAction(point);
-    }
-
-    function drawLine(start, end) {
-
-        pointLine = a.slopeIntersect(start, end);
-
-        for (point in pointLine) {
-            cm.point(point.x, point.y);
-        }
-        line = new Line(start, end);
-
-    }
-
-    function addAction(action) {
-        actions.push(action);
-        document.getElementById("actions-box").innerText +=
-            action.toString() + "\n";
-    }
-
-
-
-
-    function configure() {
-
-        // Listen for mouse clicks over canvas
-        cm.canvas.addEventListener('click', function (event) {
-            var pos = cm.getMouseCoordinates(event);
-            clickEvent(pos.x, pos.y);
-        })
-
-        // Listen for mouse movements over canvas and update info box
-        canvas.addEventListener('mousemove', function (event) {
-            var pos = cm.getMouseCoordinates(event);
-            var message = '(' + pos.x + ', ' + pos.y + ")";
-            document.getElementById('mouse-box').innerHTML = message
-
-        }, false);
-
-        // Button listeners
-        document.getElementById("axis").addEventListener("click", cm.setAxisVisibility);
-        document.getElementById("point-mode").addEventListener("click", function () {
-            drawMode = "point";
-        });
-        document.getElementById("line-mode").addEventListener("click", function () {
-            drawMode = "line";
-        });
-
-    }
 }
 
-function Algorithms() {
-
-    this.slopeIntersect = function (x1, y1, x2, y2) {
-
-        list = [];
-    
-        // If first point farther right than last one, run backwards
-        if (x1 > x2) {
-            return slopeIntersect(x2, y2, x1, y1)
-        }
-    
-        // If vertical 
-        if (x2 - x1 == 0) {
-    
-            // If first point above last one, run backwards
-            if (y1 > y2) {
-                return slopeIntersect(x2, y2, x1, y1);
-            } else {
-    
-                y = y1;
-                while (y <= y2) {
-                    list.push(x1, y);
-                    y++;
-                }
-                return list;
-            }
-    
-        } else {
-            m = (y2 - y1) / (x2 - x1);
-            b = y1 - m * x1;
-            x = x1;
-    
-            // If slope higher than 45 deg, flip axis
-            if (m > 1) {
-                return flipAxis(slopeIntersect(y1, x1, y2, x2))
-            } else {
-                while (x <= x2) {
-                    line.push(new Point(x, m * x + b));
-                    x + 1;
-                }
-            }
-        }
-    }
-    
-    function flipAxis(pointList) {
-        for (point in pointList) {
-            temp = point.x;
-            point.x = point.y;
-            point.y = temp;
-        }
-    }
-}
-
-// Point
 function Point(x, y){
-    this.x = Math.floor(x);
-    this.y = Math.floor(y);
+    this.x = x;
+    this.y = y;
     this.toString = function() {
         return "Point(" + this.x + ", " + this.y + ")";
     }
-}
-
-function Line(x1, y1, x2, y2) {
-    
 }
